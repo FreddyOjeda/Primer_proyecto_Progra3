6320d758a5154841e4cd184bf37d8553e8dc8682
Freddy Ojeda <freddy.ojeda@uptc.edu.co>
2021-03-13 09:15:01 -0500

    Primer commit

---
+++ PrimerProyecto_Progra3/src/Modelo/Word.java
@@ -0,0 +1,10 @@
+package Modelo;
+
+public class Word {
+	private String name, meaning, translation;
+	
+	public Word(String name,String meaning, String translation) {
+		this.name=name;
+		this.meaning = meaning;
+	}
+}
