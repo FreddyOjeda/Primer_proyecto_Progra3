2474cbeb7e323f8d1f0cc551993ecbead703f43a
Freddy Ojeda <freddy.ojeda@uptc.edu.co>
2021-03-13 09:28:35 -0500

    Aqui se completo la creacion de la clase word

--- PrimerProyecto_Progra3/src/Modelo/Word.java
+++ PrimerProyecto_Progra3/src/Modelo/Word.java
@@ -6,5 +6,35 @@ public class Word {
 	public Word(String name,String meaning, String translation) {
 		this.name=name;
 		this.meaning = meaning;
+		this.translation = translation;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMeaning() {
+		return meaning;
+	}
+
+	public void setMeaning(String meaning) {
+		this.meaning = meaning;
+	}
+
+	public String getTranslation() {
+		return translation;
+	}
+
+	public void setTranslation(String translation) {
+		this.translation = translation;
+	}
+	
+	@Override
+	public String toString() {
+		return name+";"+meaning+";"+translation;
 	}
 }
